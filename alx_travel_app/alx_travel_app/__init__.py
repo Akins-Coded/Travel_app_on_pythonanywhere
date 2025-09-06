e421b1f6f9e82c3e13cf5045ef8e758e8cb93ee5
Akins-Coded <akindipemuheez@gmail.com>
2025-09-06 04:14:01 +0100

    balancing

--- alx_travel_app/alx_travel_app/__init__.py
+++ alx_travel_app/alx_travel_app/__init__.py
@@ -4,7 +4,7 @@ USE_CELERY = os.getenv("USE_CELERY", "False").lower() == "true"
 
 if USE_CELERY:
     try:
-        from .celery import app as celery_app
+       # from .celery import app as celery_app
         __all__ = ("celery_app",)
     except ImportError:
         celery_app = None
--- alx_travel_app/alx_travel_app/settings.py
+++ alx_travel_app/alx_travel_app/settings.py
@@ -67,9 +67,14 @@ INSTALLED_APPS = [
     "corsheaders",
     "rest_framework",
     "drf_yasg",
-    "django_celery_results",
-    "django_celery_beat",
 ]
+
+# Only include Celery apps if enabled
+if USE_CELERY:
+    INSTALLED_APPS += [
+        "django_celery_results",
+        "django_celery_beat",
+    ]
 
 # ------------------------------------------------------------------------------
 # MIDDLEWARE
@@ -165,8 +170,9 @@ USE_TZ = True
 # ------------------------------------------------------------------------------
 # STATIC FILES
 # ------------------------------------------------------------------------------
-STATIC_URL = "/static/"
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # ------------------------------------------------------------------------------
